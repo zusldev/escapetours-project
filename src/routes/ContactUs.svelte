9f2af5aa361c9c1c9a806b4d9454da8685ad0f3f
Liborzdev <liborio07zs@gmail.com>
2023-04-22 20:16:37 -0700

    added py-20

--- src/routes/ContactUs.svelte
+++ src/routes/ContactUs.svelte
@@ -1,17 +1,20 @@
 <script>
-  import {fly} from "svelte/transition";
+  import { fly } from "svelte/transition";
   import Contact from "../lib/Contact.svelte";
   import Footer from "../lib/Footer.svelte";
   import FormContact from "../lib/FormContact.svelte";
   import NavBar from "../lib/NavBar.svelte";
 
   export let location; // svelte-routing issue
-
 </script>
 
-<NavBar/>
-<main in:fly={{ y: 200, duration: 1000 }} out:fly={{ y: 500, duration: 800 }}>
-  <FormContact/>
-  <Contact/>
-  <Footer/>
+<NavBar />
+<main
+  class="py-20"
+  in:fly={{ y: 200, duration: 1000 }}
+  out:fly={{ y: 500, duration: 800 }}
+>
+  <FormContact />
+  <Contact />
+  <Footer />
 </main>
