cfa42e9a9d10f6fef3a599aa68b53b2e8df6e3b5
zusldev-laptop <liboriozsoto@gmail.com>
2023-04-28 00:31:04 -0700

    updated profile team members

--- src/content/membersData.js
+++ src/content/membersData.js
@@ -1,34 +1,37 @@
 export const teamMembers = [
-    {
-        name: "David Zuñiga",
-        role: "Fundador",
-        bio: "David es el fundador de la empresa. A lo largo de su vida ha recorrido gran parte de Sonora y ha aprendido mucho sobre la cultura y la historia de la región.",
-        image: "https://ik.imagekit.io/escapetours/equipo/1681411609320.png?updatedAt=1681440895416",
-        instagram: "https://www.instagram.com/",
-        facebook: "https://www.facebook.com/davidalfredozunigasoto",
-        twitter: "https://twitter.com/",
-        linkedin: "https://www.linkedin.com/",
-    },
-    {
-        name: "Javier Valenzuela",
-        role: "Fundador",
-        bio: "Javier es el fundador de la empresa. A través de sus viajes ha aprendido mucho sobre la cultura y la historia de la región. Es un experto en la región y en la conducción de vehículos turísticos.",
-        image: "https://ik.imagekit.io/escapetours/equipo/1681412055807.png?updatedAt=1681440895365",
-        instagram: "https://www.instagram.com/Javiervzla30/",
-        facebook: "https://www.facebook.com/javiervzlav",
-        twitter: "https://twitter.com/",
-        linkedin: "https://www.linkedin.com/",
-    },
-    {
-        name: "Diana Félix",
-        role: "Fundadora",
-        bio: "Diana es también una de las fundadoras de la empresa. En sus viajes ha aprendido mucho sobre la cultura y la historia de la región. Le encanta la naturaleza y la aventura.",
-        image: "https://ik.imagekit.io/escapetours/equipo/1681411609339.png?updatedAt=1681440895294",
-        instagram: "https://www.instagram.com/dianaclarissa.f/",
-        facebook: "https://www.facebook.com/dianaclarissa.Fv",
-        twitter: "https://twitter.com/",
-        linkedin: "https://www.linkedin.com/",
-    },
+  {
+    name: "David Zuñiga",
+    role: "Fundador",
+    bio: "David es el fundador de la empresa. A lo largo de su vida ha recorrido gran parte de Sonora y ha aprendido mucho sobre la cultura y la historia de la región.",
+    image:
+      "https://ik.imagekit.io/escapetours/equipo/david.png?updatedAt=1682666966532",
+    instagram: "https://www.instagram.com/",
+    facebook: "https://www.facebook.com/davidalfredozunigasoto",
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/",
+  },
+  {
+    name: "Javier Valenzuela",
+    role: "Fundador",
+    bio: "Javier es el fundador de la empresa. A través de sus viajes ha aprendido mucho sobre la cultura y la historia de la región. Es un experto en la región y en la conducción de vehículos turísticos.",
+    image:
+      "https://ik.imagekit.io/escapetours/equipo/javi.png?updatedAt=1682666966438",
+    instagram: "https://www.instagram.com/Javiervzla30/",
+    facebook: "https://www.facebook.com/javiervzlav",
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/",
+  },
+  {
+    name: "Diana Félix",
+    role: "Fundadora",
+    bio: "Diana es también una de las fundadoras de la empresa. En sus viajes ha aprendido mucho sobre la cultura y la historia de la región. Le encanta la naturaleza y la aventura.",
+    image:
+      "https://ik.imagekit.io/escapetours/equipo/dani.png?updatedAt=1682666966435",
+    instagram: "https://www.instagram.com/dianaclarissa.f/",
+    facebook: "https://www.facebook.com/dianaclarissa.Fv",
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/",
+  },
 ];
 
 export default teamMembers;
